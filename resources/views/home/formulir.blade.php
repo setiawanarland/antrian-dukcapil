035edd38119fc7029ed6eaa6b48d39baae9a4493
setiawanarland <arlandmohammad@gmail.com>
2025-07-09 14:39:09 +0800

    Formulir update

--- resources/views/home/formulir.blade.php
+++ resources/views/home/formulir.blade.php
@@ -1,22 +1,28 @@
 @extends('home.mainHome')
 
 @section('container')
-    <header id="header" class="header-tops">
-
+    <div class="page-header">
         <div class="container">
-            <h2 style="color:#fff"><span class="typing" style="color:#ffbf00"></span></h2>
-
-            <div class="social-links" data-aos="fade-up" data-aos-delay="100">
-                <a href="mailto:rajaprerak@gmail.com" target="_blank" class="twitter"><i class="bi bi-twitter-x"></i></a>
-                <a href="https://www.github.com/rajaprerak" target="_blank" class="instagram"><i
-                        class="bi bi-instagram"></i></a>
-                <a href="https://www.linkedin.com/in/rajaprerak" target="_blank" class="facebook"><i
-                        class="bi bi-facebook"></i></a>
-                <a href="mailto:rajaprerak@gmail.com" target="_blank" class="tiktok"><i class="bi bi-tiktok"></i></a>
+            <div class="row">
+                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                    <div id="header" class="page-caption">
+                        <img src="dashboardAssets/img/Logo_Jeneponto.png" alt="" width="100px">
+                        <h2 class="page-title"><span class="typing" style="color:#ffbf00"></span></h2>
+                        <div class="social-links">
+                            <a href="mailto:rajaprerak@gmail.com" target="_blank" class="twitter"><i
+                                    class="bi bi-twitter-x"></i></a>
+                            <a href="https://www.github.com/rajaprerak" target="_blank" class="instagram"><i
+                                    class="bi bi-instagram"></i></a>
+                            <a href="https://www.linkedin.com/in/rajaprerak" target="_blank" class="facebook"><i
+                                    class="bi bi-facebook"></i></a>
+                            <a href="mailto:rajaprerak@gmail.com" target="_blank" class="tiktok"><i
+                                    class="bi bi-tiktok"></i></a>
+                        </div>
+                    </div>
+                </div>
             </div>
-
         </div>
-    </header>
+    </div>
     <section id="experience" class="services">
         <div class="container">
             <div class="row">
